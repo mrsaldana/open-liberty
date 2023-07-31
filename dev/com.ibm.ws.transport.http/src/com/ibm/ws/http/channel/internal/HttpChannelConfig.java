881291947580a788bdcf4c0d53fcc0916ec7a30b
mansal <mansal@us.ibm.com>
2023-07-31 10:39:03 -0400

    Delegate response processing to the pipeline WIP

--- dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/HttpChannelConfig.java
+++ dev/com.ibm.ws.transport.http/src/com/ibm/ws/http/channel/internal/HttpChannelConfig.java
@@ -1167,6 +1167,7 @@ public class HttpChannelConfig {
             if (aLog != null) {
                 this.accessLogger = aLog;
             }
+            MSP.log("access log format is set to: " + accessLogger.get().getFormat());
             Tr.debug(tc, "Config: using logging service", accessLogger);
         }
     }
